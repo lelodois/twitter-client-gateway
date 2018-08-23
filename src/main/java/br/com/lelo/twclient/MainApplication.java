185745a1e5eb6bdeda4acf199d37c0cd9cf3ed41
Léo José da Costa <leoeduar@gmail.com>
2018-08-23 18:34:57 -0300

    Spring cloud config

--- src/main/java/br/com/lelo/twclient/MainApplication.java
+++ src/main/java/br/com/lelo/twclient/MainApplication.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Bean;
 @EnableEurekaClient
 public class MainApplication {
 
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
     }
 
